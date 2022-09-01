52340ec53d0e89a1c860a9c0533ce6c13a325ae4
Felipe Sella Lopes <101262709+ViolonSella@users.noreply.github.com>
2022-09-01 08:27:34 -0300

    Criando o server e o json

---
+++ NODEJS_SERVER_HTTP/src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Felipe'
+    },
+    {
+        id: 2,
+        name: 'Felisbelto'
+    },
+    {
+        id: 3,
+        name: 'José'
+    },
+]
